7ae972d2fd648aedc33516ea4eca2052b0e9666b
chanderlier <dieser@163.com>
2020-11-16 14:40:40 +0800

    111

---
+++ studypython/yanzhengma.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+
+def generate_code(code_len=6):
+    all_chars= '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars) - 1
+    code = ''
+    for _ in range(code_len):
+        index = random.randint(0, last_pos)
+        code += all_chars[index]
+    return code
+
+if __name__ == '__main__':
+    print('code:',generate_code())
