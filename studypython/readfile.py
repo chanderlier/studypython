85c4c4b44ebee53e38c28734c6a3737a0e2b6345
chanderlier <dieser@163.com>
2020-11-18 14:05:32 +0800

    111

--- studypython/readfile.py
+++ studypython/readfile.py
@@ -1,6 +1,7 @@
+import time 
 def main():
     try:
-        with open('studypython/text.txt','r',encoding='utf-8') as f:
+        with open('studypython/text.txt', 'r', encoding='utf-8') as f:
             print(f.read())
     except FileNotFoundError:
         print('无法打开指定的文件!')
@@ -8,6 +9,16 @@ def main():
         print('指定了未知编码的!')
     except UnicodeDecodeError:
         print('读取文件时解码错误!')
+    
+    with open('studypython/text.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
+            time.sleep(0.5)
+    print()
+
+    with open('studypython/text.txt') as f:
+        lines = f.readlines()
+    print(lines)
 
 if __name__ == '__main__':
     main()
