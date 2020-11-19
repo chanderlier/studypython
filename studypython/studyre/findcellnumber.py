acfe8e2d0620aa78ca0a687b760cef0eb7098187
chanderlier <dieser@163.com>
2020-11-19 22:33:10 +0800

    111

--- studypython/studyre/findcellnumber.py
+++ studypython/studyre/findcellnumber.py
@@ -1,5 +1,6 @@
 import re
 
+
 def main():
     pattern = re.compile(r'(?<=\D)1[34578]\d{9}(?=\D)')
     sentence = '''重要的事情说三遍，我的手机号是18888888888这个靓号，不是13444343535，也不是110或119，\
@@ -16,6 +17,7 @@ def main():
     while m:
         print(m.group())
         m = pattern.search(sentence, m.end())
+
 
 if __name__ == "__main__":
     main()
